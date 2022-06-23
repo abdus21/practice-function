1ba2bce189ae347c1ce289c15d61b2a106a4c99a
samad <yy5841453@gmail.com>
2022-06-23 12:58:58 +0600

    javascript function practice

---
+++ function.js
@@ -0,0 +1,22 @@
+function inputType(a,b,...res){
+    console.log('a',a,'b',b);
+    console.log(res);
+}
+
+inputType(1,2,3,4,5,6,7,8,9);
+
+function aaaa(obj){
+    console.log(obj.name);
+}
+const obj = {
+    name : 'abdus samad',
+    age : 33
+}
+aaaa(obj)
+
+function arguments(){
+    
+    console.log( arguments);
+}
+
+arguments(1,2,3,4,5,6,7,8,9)
